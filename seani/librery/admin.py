d12f966492064a94500db9c23ddc20debfae5196
Jesher <jesherjesher1@gmail.com>
2024-02-20 14:00:41 -0600

    02 App library

---
+++ seani/librery/admin.py
@@ -0,0 +1,19 @@
+from .models import Module, Question
+from django.contrib import admin
+
+from .models import Module, Question
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'num_cuestion']
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'module', 'correct']
+    ordering = ['module']
+
---
+++ seani/librery/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Module(models.Model):
+    name = models.CharField(verbose_name="Nombre", max_length=100)
+    description = models.CharField(verbose_name="Descripción", max_length=200)
+    
+    def num_cuestion(self):
+        return self.question_set.count()
+    
+    num_cuestion.short_descrition = "Número de preguntas"
+
+
+    def __str__(self):
+        return f"{ self.name } - { self.id }"
+
+    class Meta:
+        verbose_name = 'módule'
+        verbose_name_plural = 'módulos'
+
+
+class Question(models.Model):
+    module = models.ForeignKey(
+        Module, on_delete=models.CASCADE, verbose_name="Modulo")
+    question_text = models.TextField(
+        verbose_name="Texto de la Pregunta", null=True, blank=True)
+    question_image = models.ImageField(
+        verbose_name="Imagen de la pregunta",
+        upload_to='questions',
+        null=True, blank=True)
+    answer1 = models.CharField(
+        verbose_name="Pregunta A", max_length=200)
+    answer2 = models.CharField(
+        verbose_name="Pregunta B", max_length=200)
+    answer3 = models.CharField(
+        verbose_name="Pregunta C", max_length=200, null=True, blank=True)
+    answer4 = models.CharField(
+        verbose_name="Pregunta D", max_length=200, null=True, blank=True)
+    correct = models.CharField(verbose_name="Respuesta Pregunta", max_length=5)
+
+    def __str__(self):
+        return f"{ self.module } - { self.id }"
+
+    class Meta:
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
